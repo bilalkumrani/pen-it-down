d6cd052c9dcae1d12c4f4e8b9ae6a99b298f3110
bilal <me@example.com>
2022-04-04 02:28:20 +0500

    add signin, signup routes

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signup" element={<Signin />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById("root")
 );
